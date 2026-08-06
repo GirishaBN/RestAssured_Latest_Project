d09d589a47210765be0d040348a5fb88161c5b2f
Girisha <girish@gmail.com>
2026-08-07 00:04:37 +0530

    Updateframework

--- src/test/java/tests/UserTest.java
+++ src/test/java/tests/UserTest.java
@@ -21,7 +21,7 @@ public class UserTest extends BaseTest {
 	@Test(groups= {"smoke","regression"})
 	public void createUser_shouldReturnCreatedUser()
 	{
-		UserRequest request=new UserRequest("ram11","ram@gmail11.com","male","active");
+		UserRequest request=new UserRequest("ram2","ram@gmail2.com","male","active");
 		Response response=userservice.createUser(request);
 		Assert.assertEquals(response.statusCode(), 201);
 		
